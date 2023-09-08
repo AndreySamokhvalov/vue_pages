01ebb73fbd0330f96a24fcc6eb8496eeba297eb2
Андрей Сергеевич Самохвалов <108537942+AndreySamokhvalov@users.noreply.github.com>
2023-09-08 14:39:21 +0300

    detalils

---
+++ pages-ditails/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <LinesComp />
+
+    <div class="content">
+      <HeaderComp />
+      <div class="qwe">
+        <img class="banner" alt="banner" src="@/assets/banner.jpg">
+      </div>
+      <BlogList />
+
+      <FooterComp />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import BlogList from './components/BlogList.vue';
+import HeaderComp from './components/HeaderComp.vue';
+import FooterComp from './components/FooterComp.vue';
+import LinesComp from './components/LinesComp.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    BlogList,
+    HeaderComp,
+    FooterComp,
+    LinesComp
+
+  }
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+
+.qwe {
+  width: 2400px;
+  display: flex;
+  padding-left: calc(50% - 1920px / 2);
+  padding-right: calc(50% - 1920px / 2);
+
+}
+
+
+.banner {
+  display: flex;
+  margin-bottom: 100px;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-start;
+  flex-wrap: wrap;
+
+}
+
+.center {
+  padding-left: calc(50% - 1200px / 2);
+  padding-right: calc(50% - 1200px / 2);
+
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+</style>
---
+++ pages-ditails/src/components/BlogList.vue
@@ -0,0 +1,213 @@
+<template>
+    <div class="blog__list_elem center">
+        <div class=" blog__list">
+            <ol style="list-style-type: none">
+                <BlogItem v-for="item in blogs" :item-data="item" :key="item.id" class="blog__list__item"></BlogItem>
+            </ol>
+        </div>
+
+        <div class="btns">
+            <h1 class="">Tags</h1>
+            <div class="tags__button">
+                <button @click="sortTag = 'kitchen', filterBlogs(), toggleUse1()" class="tags__button_btn"
+                    :class="{ btnActiv: toggle1 }">Kitchen</button>
+                <button @click="sortTag = 'bathroom', filterBlogs(), toggleUse2()" class="tags__button_btn"
+                    :class="{ btnActiv: toggle2 }">Bathroom</button>
+            </div>
+            <div class="tags__button">
+                <button class="tags__button_btn">Building</button>
+                <button class="tags__button_btn">Architecture</button>
+            </div>
+            <div class="tags__button">
+                <button class="tags__button_btn">Kitchen Planning</button><button class="tags__button_btn">Bedroom</button>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import BlogItem from '@/components/BlogItem.vue';
+export default {
+    name: 'BlogList',
+    components: {
+        BlogItem,
+    },
+
+    data() {
+        return {
+            sortTag: "",
+            blogs: [
+                { id: '1', tag: 'kitchen', blogName: 'Let’s Get Solution for Building Construction Work', url: './img/main/kitchen.png', blogInfo: '26 December,2022', blogText: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable .Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary." },
+                { id: '2', tag: 'bathroom', blogName: "Let's find a solution for the construction work in the building", url: './img/main/bath2.png', blogInfo: '26 December,2022', blogText: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable .Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary." }
+            ],
+            reserve: [],
+            toggle1: false,
+            toggle2: false,
+        };
+    },
+
+    methods: {
+        toggleUse1() {
+
+            if (!this.toggle1) {
+                this.toggle1 = !this.toggle1;
+                this.toggle2 = !this.toggle1;
+            } else {
+
+                this.toggle2 = !this.toggle1;
+            }
+
+        },
+        toggleUse2() {
+
+            if (!this.toggle2) {
+                this.toggle2 = !this.toggle2;
+                this.toggle1 = !this.toggle2;
+            } else {
+
+                this.toggle1 = !this.toggle2;
+            }
+        },
+
+
+        filterBlogs() {
+            if (this.reserve.length == 0) {
+                this.reserve = Array.from(JSON.parse(JSON.stringify(this.blogs)));
+
+                switch (this.sortTag) {
+                    case "kitchen":
+                        this.blogs = this.blogs.filter(item => item.tag === this.sortTag);
+                        break;
+                    case "bathroom":
+                        this.blogs = this.blogs.filter(item => item.tag === this.sortTag);
+
+                }
+            } else {
+                this.blogs = Array.from(JSON.parse(JSON.stringify(this.reserve)));
+
+                switch (this.sortTag) {
+                    case "kitchen":
+                        this.blogs = Array.from(JSON.parse(JSON.stringify(this.blogs)).filter(item => item.tag === this.sortTag));
+                        break;
+                    case "bathroom":
+                        this.blogs = Array.from(JSON.parse(JSON.stringify(this.blogs)).filter(item => item.tag === this.sortTag));
+                }
+            }
+        }
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.blog__list_elem {
+
+    width: 1200px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    flex-wrap: wrap;
+}
+
+
+.component__none {
+    display: none;
+}
+
+.tags {
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    width: 298px;
+    gap: 12px;
+
+    &__button {
+        margin-top: 10px;
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+
+        &_btn {
+            display: flex;
+            padding: 9px 30px;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+            border-radius: 10px;
+            background: #F4F0EC;
+
+        }
+
+        &_btn:active {
+            background: #292F36;
+            color: #F4F0EC;
+        }
+    }
+}
+
+.btnActiv {
+    display: flex;
+    padding: 9px 30px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 10px;
+    background: #F4F0EC;
+    background: #292F36;
+    color: #F4F0EC;
+}
+
+.blog__list {
+
+    display: flex;
+    width: 802px;
+
+    &__item {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        margin-bottom: 100px;
+
+        &_title {
+            display: flex;
+            color: $DM_Serif_Display_color;
+            font-family: DM Serif Display;
+            font-size: 50px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 125%;
+            letter-spacing: 1px;
+            margin-bottom: 20px;
+        }
+
+        &_info {
+            margin-top: 46px;
+            display: flex;
+            flex-direction: row;
+            margin-bottom: 46px;
+
+            &_right {
+                margin-left: 410px;
+            }
+        }
+
+        &_description {
+            display: flex;
+            color: $jost;
+            font-family: Jost;
+            font-size: 22px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 150%;
+            letter-spacing: 0.22px;
+
+        }
+    }
+
+}
+</style>
+
+
